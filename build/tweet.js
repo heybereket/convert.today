0549ae8f33d3ae29a44e641555f6b944c919ef38
Bereket Semagn <semagnbereket@gmail.com>
2021-02-02 11:33:26 -0500

    first build completed

---
+++ build/tweet.js
@@ -0,0 +1,3 @@
+function tweet(){
+    window.open(`https://twitter.com/intent/tweet?text=Apply%20cool%20effects%20to%20your%20images%20with%20https://www.convert.today%20by%20@heybereket`, '_blank', 'location=yes,height=570,width=520,scrollbars=yes,status=yes');
+}
